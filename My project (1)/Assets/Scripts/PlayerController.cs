d77dc8106be448c47b9a460a853d861a91239ead
Foster Boone <10787343@uvu.edu>
2022-03-22 16:32:51 -0600

    Animal movement ad deletion

--- My project (1)/Assets/Scripts/PlayerController.cs
+++ My project (1)/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float xRange = 10;
     public float speed = 10.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a Projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         // Keep Player In Bounds
         if (transform.position.x > xRange)
         {
