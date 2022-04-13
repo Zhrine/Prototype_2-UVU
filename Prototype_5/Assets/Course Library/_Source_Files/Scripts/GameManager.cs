df8258714a7751d8d3cd81f74be544e124c1d694
Foster Boone <10787343@uvu.edu>
2022-04-13 00:07:26 -0600

    Final Prototype Finished

--- Prototype_5/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
+++ Prototype_5/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
@@ -2,24 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
+    public GameObject titleScreen;
+    public Button restartButton;
     public bool isGameActive;
     public TextMeshProUGUI gameOverText;
     private int score;
     public TextMeshProUGUI scoreText;
     public List<GameObject> targets;
+    
 
     private float spawnRate = 1.0f;
     
     // Start is called before the first frame update
-    void Start()
+    public void StartGame(int difficulty)
     {
         isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
+        spawnRate /= difficulty;
         UpdateScore(0);
-    } 
+        titleScreen.gameObject.SetActive(false);
+    }
+    
     IEnumerator SpawnTarget()
     {
         while(true)
@@ -40,5 +48,11 @@ public class GameManager : MonoBehaviour
    {
        isGameActive = false;
        gameOverText.gameObject.SetActive(true);
+       restartButton.gameObject.SetActive(true);
+   }
+
+   public void RestartGame()
+   {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
    }
 }
