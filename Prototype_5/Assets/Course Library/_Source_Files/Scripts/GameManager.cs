72e6a8b1533381bb5506b47381ef60692c7fc9ef
Foster Boone <10787343@uvu.edu>
2022-04-12 22:47:52 -0600

    In between Work

---
+++ Prototype_5/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    
+}
